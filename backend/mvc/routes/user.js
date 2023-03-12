3b3b9b23cfdc5a98809a1db8282b7edcbe94a324
zeeshan117 <zeeshan.m@darwinbox.io>
2023-03-12 22:55:54 +0530

    re-push

--- backend/mvc/routes/user.js
+++ backend/mvc/routes/user.js
@@ -7,16 +7,14 @@ const allUsers=require('../controllers/user/fetchAll')
 const validate=require('../controllers/user/validate')
 const addUser=require('../controllers/user/post')
 const userUpdate = require('../controllers/user/userProfile')
-const userSearch=require('../controllers/search/usersearch')
 const userCount = require('../controllers/user/count')
 routes.get('/fetchUser/:email',userDetails.fetchUser)
-routes.get('/userSearch',userSearch.getUserDetails)
 routes.get('/userCount',userCount.countUsers);
 routes.get('/fetchAllUsers',allUsers.fetchAllUsers)
 
 routes.post('/validate',validate.validate)
 routes.post('/addUser',addUser.addUser)
-// routes.post('/userUpdate/:email',uploadImage.single("profile"),userUpdate.updateUser)
+routes.post('/userUpdate/:email',userUpdate.updateUser)
 
 
 module.exports = routes;    
